7626f50320d9ce76928b8038d6f9c2a78228e414
Bianca <bibca17@hotmail.com>
2016-08-09 15:56:46 -0300

    adicionando a classe circulo

---
+++ src/Circulo.java
@@ -0,0 +1,11 @@
+public class Circulo implements AreaCalculavel{
+	double raio;
+
+	public Circulo( double raio){
+		this.raio = raio;
+	}
+	
+	public double calculaArea(){
+		return Math.PI * raio * raio;
+	}
+}
